4dbffd5d55ee66f8d5750429560c44a8c4169e46
opilane <opilane@HP-CZC6297B89>
2022-03-22 12:20:05 +0200

    Add project files.

---
+++ MobileAppStart/pickerr.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppStart
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pickerr : ContentPage
+    {
+        Picker picker;
+        WebView webView;
+        StackLayout st;
+        string[] lehed = new string[4] { "https://tahvel.edu.ee/#/", "https://moodle.edu.ee/my/", "https://www.tthk.ee/", "" };
+        public pickerr()
+        {
+            picker = new Picker
+            {
+                Title = "Webilehed"
+            };
+            picker.Items.Add("Tahvel");
+            picker.Items.Add("Moodle");
+            picker.Items.Add("TTHK");
+            picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+            webView = new WebView
+            { };
+            SwipeGestureRecognizer swipe = new SwipeGestureRecognizer();
+            swipe.Swiped += Swipe_Swiped;
+            swipe.Direction = SwipeDirection.Right;
+            webView.GestureRecognizers.Add(swipe);
+            st = new StackLayout { Children = { picker } };
+            Content = st;
+
+
+
+            var entry = new Entry
+            {
+                Placeholder = "Please Enter Some Text Here",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Keyboard = Keyboard.Email
+            };
+        }
+
+        private void Swipe_Swiped(object sender, SwipedEventArgs e)
+        {
+            webView.Source = new UrlWebViewSource { Url = lehed[4] };
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (webView!=null)
+            {
+                st.Children.Remove(webView);
+            }
+            webView = new WebView
+            {
+                Source = new UrlWebViewSource { Url = lehed[picker.SelectedIndex]} ,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+            };
+            st.Children.Add(webView);
+        }
+    }
+}
---
+++ MobileAppStart/trips.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppStart
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class trips : ContentPage
+    {
+        BoxView[,] boxs = new BoxView[5, 5];
+        int i, j;
+        Image img;
+        public trips()
+        {
+            this.BackgroundColor = Color.Black;
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+
+                }
+            };
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+
+                    img = new Image { Source = ImageSource.FromFile("index.png") };
+
+                    grid.Children.Add(img, i, j);
+                    var tap = new TapGestureRecognizer();
+
+                    img.GestureRecognizers.Add(tap);
+                    tap.Tapped += async (object sender, EventArgs e) =>
+                    {
+
+                        Image img = sender as Image;
+                        if (img.Source == ImageSource.FromFile("nolik.png"))
+                        {
+                            img.Source = ImageSource.FromFile("krestik.png");
+                        }
+                        else
+                        {
+                            img.Source = ImageSource.FromFile("nolik.png");
+                        }
+                    };
+                }
+            }
+            Content = grid;
+        }
+    }
+}
